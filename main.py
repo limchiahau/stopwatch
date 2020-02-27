f5f5fe7a7a6a305be81d9ad46ecb50f4325145cf
chl <chl@chl.com>
2020-02-27 14:43:48 +0800

    removed unecessary comments from main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from listener import KeyboardListener
 def minutes(mins):
     return mins * 60
 
-#s = Stopwatch(minutes(1))
 s = LappedStopwatch(minutes(30),minutes(1))
 alt = Alternator(lambda: s.pause(), lambda: s.resume())
 
@@ -13,6 +12,3 @@ s.start()
 
 listener = KeyboardListener()
 listener.on_enter(alt.toggle)
-
-#keyboard.f._._.f.f._.f.e.f._._.f.e.f._._.g.a.g
-#keyboard.f._._.f.f._.f.e.f._._.f.c
